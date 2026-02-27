90bca433e8327e19f118e33cee4171aef667ce71
Willey <willeynascimentodev@gmail.com>
2026-02-26 21:36:40 -0300

    feat(auth): User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,28 +2,20 @@
 
 namespace Database\Factories;
 
-use App\Models\User;
+use App\Domain\User\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
     protected $model = User::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
+    public function definition(): array
     {
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name'     => $this->faker->name(),
+            'email'    => $this->faker->unique()->safeEmail(),
+            'password' => Hash::make('password'),
         ];
     }
 }
